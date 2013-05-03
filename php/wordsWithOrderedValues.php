da8232d5809ceb4737fe106a617c8c5acb034f58
Mike <dunton@cs.fsu.edu>
2013-05-03 06:04:06 +0400

    Added a php script that finds words with ordered vowels

--- php/wordsWithOrderedValues.php
+++ php/wordsWithOrderedValues.php
@@ -1,17 +1,31 @@
 <?php
+/*#############################################################################
+*Find words in a word list that contain all the owels in alphabetical order
+*non-repeated, where vowels are defined as A E I O U Y
+*##############################################################################
+*/
 
 function aeiouy($lines)
 {
-  $words = array();
+  
   foreach ($lines as $word)
   {
-    preg_replace('/^aeiouy/', '', $word);
-    echo "$word \n";
-    if($word == 'aeiouy')  
-      $words.append($word);
-  }  
-  return $words;
+		$vowelCheck = '';
+		foreach(str_split($word) as $char)
+		{
+			if(in_array($char, str_split("aeiouy")))
+			{
+				$vowelCheck = $vowelCheck.$char;
+			}
+		}
+		if(strtolower($vowelCheck) == "aeiouy")
+		{
+			echo $word;
+		}
+	}
 }
-$lines = file($argv[1]);
 
-print_r(aeiouy($lines));
+$lines = file($argv[1]);
+aeiouy($lines);
+
+?>
